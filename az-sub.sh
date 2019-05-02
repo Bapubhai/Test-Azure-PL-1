5e439550b0b890ef8f7fb01f35212ac788e22ae3
Bhai <bdellikerrao@gmail.com>
2019-05-02 11:44:56 -0700

    [skip-ci]

--- az-sub.sh
+++ az-sub.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #test it
 
-az group create --name bhai-rg1 --location westus --subscription 8c6de12e-4b49-41c0-8eff-35ac89686cf4
+az group delete --name bhai-rg1 --location westus --subscription 8c6de12e-4b49-41c0-8eff-35ac89686cf4
